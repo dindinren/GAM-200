6ab208b8a5f31786db1f726bd6c54d57fe33d6e9
dindinren <joycelynb21@gmail.com>
2025-09-05 00:18:47 +0800

    Character and Camera Movement
    + Character move left and right
    + Camera will follow

--- Assets/PlayerManagement.cs
+++ Assets/PlayerManagement.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-
+using System.Collections;
 public class PlayerManagement : MonoBehaviour
 {
+    public GameObject player;
+    public float speed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        var horizontalinput = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movement = new Vector3(horizontalinput, 0, 0);
+
+        transform.Translate(movement *  speed * Time.deltaTime);
+
+    }
+
 }
